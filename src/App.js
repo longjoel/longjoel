8fb4dcbe892a10ba6b57a45a915a0d35cdd318bc
Joel Longanecker <joel.longanecker@gmail.com>
2020-10-25 13:38:01 -0500

    looking good :D

--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
 import Marked from 'marked'
+import jobs from './workHistory.json';
 
 const MD = ({ text }) =>
   <div dangerouslySetInnerHTML={{ __html: Marked.parse(text) }}></div>
 
 
 // A fun little divider for the top
-const AppBarDivider = () => <span style={{ marginLeft: '16px', marginRight: '8px' }}><h4>|</h4></span>
+const AppBarDivider = () => <span style={{ marginLeft: '16px', marginRight: '4px' }}><h4>|</h4></span>
 
 const AppBarItem = ({ children, location = '' }) => {
-  return <h4 class="navbar-text"><a href={`#${location}`}>{children}</a></h4>
+  return <span class="navbar-text navbar-item"><a href={`#${location}`}>{children}</a></span>
 
 }
 
-const AppBar = () => <div class="navbar navbar-expand-lg navbar-light bg-light sticky-top" >
+const AppBar = () => <div class="navbar navbar-expand navbar-light bg-light sticky-top" >
   <AppBarItem ><b>Joel Longanecker</b></AppBarItem> <AppBarDivider />
-  <AppBarItem location="projects" >Projects</AppBarItem> <AppBarDivider />
-  <AppBarItem location="contact" >Contact</AppBarItem> <AppBarDivider />
-  <AppBarItem location="education" >Education</AppBarItem> <AppBarDivider />
-  <AppBarItem location="workHistory" >Work History</AppBarItem>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <AppBarItem location="projects" >Projects</AppBarItem> <AppBarDivider />
+    <AppBarItem location="contact" >Contact</AppBarItem> <AppBarDivider />
+    <AppBarItem location="education" >Education</AppBarItem> <AppBarDivider />
+    <AppBarItem location="workHistory" >Work History</AppBarItem>
+  </div>
+
 </div>
 
 const PageSection = ({ id, title, caption, children }) => {
-  return <div className="container" >
-    <div className="row">
-      <div className="col-sm-3">
-        <div>
-      <h3 id={id}>{title}</h3>
-      <h5><em>{caption}</em></h5>
-      </div>
-      </div>
-      <div className="col-sm-9">
-        <br/>
-      {children}
+
+  if (title || caption) {
+    return <div className="container m-3 p-3 bg-light rounded"  >
+      <div className="row">
+        <div className="col-md-3 col-sm-12">
+          <div>
+            <h3 ><a class="text-dark" id={id} href={`#${id}`}>{title}</a></h3>
+            <h5><em>{caption}</em></h5>
+          </div>
+        </div>
+        <div className="col-md-9 col-sm-12">
+
+          {children}
+
+        </div>
+
 
       </div>
-    
-  
     </div>
+  }
+  else {
+    return <div className="container m-3 p-3 bg-light rounded"  >
+      <div className="row">
+        <div className="col-sm-12">
+
+          {children}
+
+        </div>
+      </div>
     </div>
+  }
+
 }
 
-const AboutMe = ()=> <PageSection id="" title="About Me" caption="Thank you for visiting my GH Page.">
-  <MD text={`
-Hello! My name is Joel Longanecker. Thank you for visiting my Github page. 
+const AboutMe = () => <PageSection id="">
+  <MD text={`Hello! My name is Joel Longanecker. Thank you for visiting my Github page. 
 I made this page to serve several different purposes.
 
 * This is my living resume! It's way easier to show people you are a web developer than simply telling them.
@@ -54,16 +73,52 @@ I made this page to serve several different purposes.
 
 function App() {
 
+  const WorkHistory = ()=> <PageSection id="workHistory" title="Work History" caption="Is a man not entitled to the sweat of his brow?">
+    {jobs.map(job => {
+      return <div id={job.name}>
+
+        <h3>{job.name}</h3>
+        <em>{job.dateStart} to {job.dateEnd}</em>
+        <h4>{job.summary}</h4>
+        <div className="row">
+          <div className="col-md-4 col-sm-12">
+            <div>Roles:</div>
+            <ul>
+              {job.roles.map(role => <li>{role}</li>)}
+            </ul>
+            <div>Teams:</div>
+            <ul>
+              {job.teams.map(team => <li>{team}</li>)}
+            </ul>
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <div>Tools:</div>
+            <ul>
+              {job.tools.map(tool => <li>{tool}</li>)}
+            </ul>
+          </div>
+        </div>
+      </div>;
+    })}
+  </PageSection>;
   return (
-    <div className="App">
+    <div className="App bg-dark" style={{ height: '100%' }}>
       <AppBar />
-     <AboutMe/>
-     <br/>
+      <AboutMe />
       <PageSection id="projects" title="Projects" caption="Things I have worked on independently.">
         <ul>
           <li>Bad Bits Game Engine</li>
         </ul>
       </PageSection>
+      <PageSection id="contact" title="Contact" caption="Hey! I'm over here!">
+
+      </PageSection>
+
+      <PageSection id="education" title="Education" caption="I managed to convince them to give me a degree...">
+
+      </PageSection>
+
+      <WorkHistory/>
     </div>
   );
 }
